f4d673733ec7699e624f956f5428a95d88f536f6
GregCatalano <greg@it-all.com>
2017-09-16 13:55:50 -0400

    remove unused fn

--- Src/Infrastructure/Utilities/ErrorHandler.php
+++ Src/Infrastructure/Utilities/ErrorHandler.php
@@ -297,23 +297,4 @@ class ErrorHandler
 
         return $out;
     }
-
-    // http://php.net/manual/en/function.debug-backtrace.php#112238
-    private function getCallTrace(string $traceString)
-    {
-        $trace = explode("\n", $traceString);
-        // reverse array to make steps line up chronologically
-        $trace = array_reverse($trace);
-        array_shift($trace); // remove {main}
-        array_pop($trace); // remove call to this method
-        $length = count($trace);
-        $result = array();
-
-        for ($i = 0; $i < $length; $i++)
-        {
-            $result[] = ($i + 1)  . ')' . substr($trace[$i], strpos($trace[$i], ' ')); // replace '#someNum' with '$i)', set the right ordering
-        }
-
-        return "\t" . implode("\n\t", $result);
-    }
 }
