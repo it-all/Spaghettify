084613aa6731bf984b683d0ddb41f2c16bfe62bc
GregCatalano <greg@it-all.com>
2017-09-20 09:16:36 -0400

    make ready for composer package

--- Src/init.php
+++ Src/init.php
@@ -160,3 +160,27 @@ if (!Utilities\isRunningFromCommandLine()) {
     session_start();
     $_SESSION[SESSION_LAST_ACTIVITY] = time(); // update last activity time stamp
 }
+
+// Instantiate Slim PHP
+$settings = require APP_ROOT . 'config/slim3/settings.php';
+$slim = new \Slim\App($settings);
+
+$container = $slim->getContainer();
+
+// Set up Slim dependencies
+require APP_ROOT . 'config/slim3/dependencies.php';
+
+// remove Slim's Error Handling
+unset($container['errorHandler']);
+unset($container['phpErrorHandler']);
+
+// Middleware registration
+// handle CSRF check failures and allow Twig to access and insert CSRF fields to forms
+$slim->add(new \It_All\Spaghettify\Src\Infrastructure\Security\CsrfMiddleware($container));
+// slim CSRF check middleware
+$slim->add($container->csrf);
+
+// Register routes
+require APP_ROOT . 'routes.php';
+
+$slim->run();
--- Src/public/index.php
+++ Src/public/index.php
@@ -2,27 +2,3 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../init.php';
-
-// Instantiate Slim PHP
-$settings = require APP_ROOT . 'config/slim3/settings.php';
-$slim = new \Slim\App($settings);
-
-$container = $slim->getContainer();
-
-// Set up Slim dependencies
-require APP_ROOT . 'config/slim3/dependencies.php';
-
-// remove Slim's Error Handling
-unset($container['errorHandler']);
-unset($container['phpErrorHandler']);
-
-// Middleware registration
-// handle CSRF check failures and allow Twig to access and insert CSRF fields to forms
-$slim->add(new \It_All\Spaghettify\Src\Infrastructure\Security\CsrfMiddleware($container));
-// slim CSRF check middleware
-$slim->add($container->csrf);
-
-// Register routes
-require APP_ROOT . 'routes.php';
-
-$slim->run();
